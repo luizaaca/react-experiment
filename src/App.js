ca3ed9de8b75598d968f94f4b1e3b921134dbb06
Luiz Assis <luiz.assis-ext@viavarejo.com.br>
2020-03-17 10:49:52 -0300

    class -> function: using hooks on app function

--- src/App.js
+++ src/App.js
@@ -1,55 +1,53 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import Person from './Person/Person'
 
-class App extends Component {
-  state = {
+const app = props => {
+  const [personsState, setPersonsState] = useState({
     persons: [
       { name: 'Max', age: 30 },
       { name: 'Mane', age: 25 },
       { name: 'John', age: 32 }
     ]
-  }
+  })
 
-  switchNameHandler = () => {
-    this.setState({
-      persons: [
-        { name: 'Maximilian', age: 35 },
-        { name: 'Mane', age: 25 },
-        { name: 'John', age: 32 }
-      ]
-    })
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React...</h1>
-        </header>
-        <button onClick={this.switchNameHandler}>Change Name</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}>
-          My Hobbies: Trekking
-        </Person>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    )
-    // return React.createElement('div', null, 'h1', 'Hello world!!!');
-  }
+const switchNameHandler = () => {
+  setPersonsState({
+    persons: [
+      { name: 'Maximilian', age: 35 },
+      { name: 'Mane', age: 25 },
+      { name: 'John', age: 32 }
+    ]
+  })
 }
 
-export default App
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React...</h1>
+      </header>
+      <button onClick={switchNameHandler}>Change Name</button>
+      <Person
+        name={personsState.persons[0].name}
+        age={personsState.persons[0].age}
+      />
+      <Person
+        name={personsState.persons[1].name}
+        age={personsState.persons[1].age}
+      />
+      <Person
+        name={personsState.persons[1].name}
+        age={personsState.persons[1].age}>
+        My Hobbies: Trekking
+      </Person>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+  )
+  // return React.createElement('div', null, 'h1', 'Hello world!!!');
+}
+
+export default app
