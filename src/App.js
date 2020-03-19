a5cc23fb595d12f0b0f69f7c2f410caecb4717ec
Luiz Assis <luizaaca@gmail.com>
2020-03-19 09:17:27 -0300

    two-way binding props inside view + styling

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Person from "./Person/Person";
 class App extends Component {
   state = {
     persons: [
-      { name: "Max", age: 30 },
+      { name: "Maxi", age: 30 },
       { name: "Mane", age: 25 },
       { name: "John", age: 32 }
     ]
@@ -22,6 +22,16 @@ class App extends Component {
     });
   };
 
+  nameChangedHandler = event => {
+    this.setState({
+      persons: [
+        { name: "Maxi", age: 35 },
+        { name: event.target.value, age: 25 },
+        { name: "John", age: 32 }
+      ]
+    });
+  };
+
   render() {
     return (
       <div className="App">
@@ -29,29 +39,26 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React...</h1>
         </header>
-        <button
-          onClick={/*don't use this way*/ () => this.switchNameHandler("Joan")}
-        >
+        <button onClick={this.switchNameHandler.bind(this, "José")}>
           Change Name
         </button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
+          changed={this.nameChangedHandler}
         />
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, "José")}
+          changed={this.nameChangedHandler}
         />
         <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+          changed={this.nameChangedHandler}
         >
           My Hobbies: Trekking
         </Person>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
     // return React.createElement('div', null, 'h1', 'Hello world!!!');
