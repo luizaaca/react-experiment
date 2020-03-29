8d5cd97d457daf127a7bc19cfe6e68a876088892
Luiz Assis <luizaaca@gmail.com>
2020-03-29 13:30:31 -0300

    Memo and Fragment

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -19,4 +19,4 @@ const cockpit = props => {
   );
 };
 
-export default cockpit;
+export default React.memo(cockpit);
--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { Fragment } from "react";
 import classes from "./Person.css";
 
 const person = props => (
-  <div className={classes.Person}>
-    <p onClick={props.click}>
-      I 'm {props.name} and I' m {props.age} years old! - {props.children}
-    </p>
-    <input type="text" onChange={props.changed} value={props.name} />
-  </div>
+  <Fragment>
+    <div className={classes.Person}>
+      <p onClick={props.click}>
+        I 'm {props.name} and I' m {props.age} years old! - {props.children}
+      </p>
+      <input type="text" onChange={props.changed} value={props.name} />
+    </div>
+  </Fragment>
 );
 
 export default person;
